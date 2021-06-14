2ac570b034b3b5911c32b46f3cee2f8b0f1603d6
Charvisaxena01 <74494231+Charvisaxena01@users.noreply.github.com>
2021-06-14 11:44:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint
+var engine,world;
+
+var divisionHeight = 300;
+var plinkos = []
+var divisions = []
+var particles = []
+
+
+
+function setup() {
+  createCanvas(485,800);
+  engine = Engine.create();
+	world = engine.world;
+  ground = new Ground(0,800,980,30)
+  for(var j = 40;j<=width; j=j+50)
+  {
+      plinkos.push(new plinko(j,75))
+  }
+  for(var j = 40;j<=width; j=j+50)
+  {
+      plinkos.push(new plinko(j,175))
+  }
+  for(var j = 40;j<=width; j=j+50)
+  {
+      plinkos.push(new plinko(j,275))
+  }
+  for(var j = 40;j<=width; j=j+50)
+  {
+      plinkos.push(new plinko(j,375))
+  }
+  for(var k= 0; k<=width;k=k+80){
+    divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight))
+  }
+
+}
+
+function draw() {
+  background("lightGreen");  
+  drawSprites();
+  ground.display()
+  for(var j=0; j<plinkos.length;j++)
+  {
+    plinkos[j].display()
+  }
+  for(var k =0;k<divisions.length;k++){
+    divisions[k].display()
+  }
+  if(frameCount %60 === 0){
+    particles.push(new Particle(random(width/2-30,width/2+30),10,10))
+  }
+  for(var m = 0; m<particles.length;m++){
+    particles[m].display()
+  }
+}
